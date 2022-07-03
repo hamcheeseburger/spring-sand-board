9677cdebcde36bd02dc6c6018153f8876f7ebef7
yangdongjue5510 <djwhy5510@gmail.com>
2022-07-03 14:57:34 +0900

    configure: 프로젝트 생성

---
+++ sandboard/src/main/java/yangdongjue5510/sandboard/SandboardApplication.java
@@ -0,0 +1,13 @@
+package yangdongjue5510.sandboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SandboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SandboardApplication.class, args);
+    }
+
+}
---
+++ sandboard/src/test/java/yangdongjue5510/sandboard/SandboardApplicationTests.java
@@ -0,0 +1,13 @@
+package yangdongjue5510.sandboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SandboardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
